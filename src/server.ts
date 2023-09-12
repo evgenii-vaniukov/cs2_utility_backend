71142f947a05423529c2ccce18fab5572e6fa0ef
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2023-09-12 22:49:36 +0300

    Removes morgan.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import express from "express";
-import morgan from "morgan";
 import router from "./utility_collections/collections.router";
 
 const app = express();
 
-app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
