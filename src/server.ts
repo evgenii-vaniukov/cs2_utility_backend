6518bf63d577d7ce3acd4ff59e64d9924a662c5a
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2023-09-25 16:28:55 +0300

    Removes analytics endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import cors from "cors";
 import express from "express";
 import utilities_router from "./grenades/router";
-import likes_router from "./likes_count/router";
 import utility_collections_router from "./utility_collections/router";
 
 const app = express();
@@ -17,7 +16,6 @@ app.use(express.urlencoded({extended: true}));
 
 app.use("/utilitycollections", utility_collections_router);
 app.use("/utilities", utilities_router);
-app.use("/likes", likes_router);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
